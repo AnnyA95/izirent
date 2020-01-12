e7e6fb11436b875d872ba7f02c3a2580f711c959
Anny Aidinian <anny.aidinian@finalist.nl>
2020-01-12 15:46:34 +0100

    email template

--- Website/js/checkout.js
+++ Website/js/checkout.js
@@ -102,28 +102,56 @@ if(navi ==  null){
 addComment();
 
 
+
+
 function sendMail() {
   debugger
-  var productname = $('#current').html();
+  var pickupdatestart = document.getElementById('begind').innerHTML;
+  var pickupdatestarttime = document.getElementById('begint').innerHTML;
+  var pickupdateend = document.getElementById('eindd').innerHTML;
+  var pickupdateendtime = document.getElementById('eindt').innerHTML;
+  var days = document.getElementById('totaldays').innerHTML;
+  var km = document.getElementById('km').innerHTML;
+  var price = document.getElementById('price').innerHTML;
+  var anwb = document.getElementById('1').innerHTML;
+  var insure1 = document.getElementById('2').innerHTML;
+  var insure2 = document.getElementById('3').innerHTML;
+  var driver = document.getElementById('4').innerHTML;
+  var wheels = document.getElementById('5').innerHTML;
+  var nav = document.getElementById('6').innerHTML;
+  var productname = $('#product').html();
   var email = $('#email').val();
   var name = $('#name').val();
   var lastname = $('#lastname').val();
+  var country = $('#country').val();
   var street = $('#street').val();
   var zipcode = $('#zipcode').val();
   var city = $('#city').val();
   var phone = $('#phone').val();
-  var pickup = $('#pickup').val();
-  var returndate = $('#returndate').val();
   var payment =   $('#payment').val();
   var message = $('#info').val();
   var valid = this.emailValidate(email)
+  
   if(valid){
-    window.location.href = 'mailto:anni032@hotmail.com?subject=The subject - ' + productname + ' (' + email + ')' + '&body=' + "Naam" + "%3A%20" + name + message + pickup + "test" +  city; 
-    $('#myModal').modal('hide');
+    window.location.href = 'mailto:anni032@hotmail.com?subject=The subject - ' + productname + ' (' + email + ')' + '&body=' + 
+    "PERSONALINFO" + "%3A%20" + "%0D%0A" +  "Name:" + "%20"  + name  +  "%0D%0A" +  "Lastname:" + "%20" + lastname   +  "%0D%0A" +  "Phone:" + "%20" +  phone + "%0D%0A" + 
+    "%0D%0A" + "ADRES" + "%3A%20" +   "%0D%0A" +  "Country:" + "%20"  + country  +  "%0D%0A" +  "City:" + "%20" + city   +  "%0D%0A" +  "Street:" + "%20" +  street +   "%0D%0A" +  "zipcode:" + "%20" +  zipcode +
+    
+    
+   "%0D%0A" +  "%0D%0A" +  "CARINFO:" + "%20" +  "%0D%0A"   + 'Pick-up date:' + "%20"  + pickupdatestart  +  "%0D%0A" +  "Pick-up time:" + "%20" + pickupdatestarttime   +  "%0D%0A" +  "Returndate:" + "%20" +  pickupdateend +   "%0D%0A" +  "Return time:" + "%20" +  pickupdateendtime +
+    "%0D%0A" +  "Price:" + "%20" +  price +   "%0D%0A" +  "Days:" + "%20" +  days +  "%0D%0A" +  "Km:" + "%20" +  km +
+   
+
+    
+    "%0D%0A" + "%0D%0A" + 
+    "EXTRAS" +  "%3A%20" + "%0D%0A" +  "-" + anwb  +  "%0D%0A" +  "-" + insure1 + "%0D%0A" +  "-" + insure2 +  "%0D%0A" + "-" + driver  + "%0D%0A" + "-" + wheels + "%0D%0A" + "-" + nav
+
+    event.preventDefault()
     this.redirect()
-    alert(payment)
+    
   }else{
-    alert("email is fout")
+    event.preventDefault()
+  
   }
 
 };
